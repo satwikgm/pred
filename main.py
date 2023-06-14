23037872fa8e8787f236a43f195a864d12746fb9
unknown <satwikmishra06092001@gmail.com>
2023-06-14 12:42:38 +0530

    fc

---
+++ main.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from flask import Flask, render_template , request
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+df = pd.read_csv('Cleaned_bangalore_house_data.csv')
+pipe = pickle.load(open('LRModel.pkl','rb'))
+
+
+@app.route('/')
+def index():
+    locations = sorted(df['location'].unique())
+    return render_template('index.html',locations=locations)
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    location = request.form.get('location')
+    bhk = request.form.get('bhk')
+    bath = request.form.get('bath')
+    total_sqft = request.form.get('total_sqft')
+
+    print(location , bhk , bath , total_sqft)
+
+    input = pd.DataFrame([[location , total_sqft , bath , bhk]] , columns=['location' , 'total_sqft' , 'bath' , 'bhk'])
+
+    prediction = pipe.predict(input)[0]*(1e5)
+
+    return str(np.round(prediction,2))
+
+if __name__ == '__main__':
+    app.run(debug=True , port=5000)
